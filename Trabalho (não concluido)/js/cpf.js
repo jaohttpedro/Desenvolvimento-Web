7113088cd5a581acee3fbf269deeec1c803a9646
João Hoffmann <hoffmanndasilvaj@gmail.com>
2025-07-24 16:05:36 -0300

    Trabalho que explora o básico de web

---
+++ Trabalho (não concluido)/js/cpf.js
@@ -0,0 +1,6 @@
+//apenas para manter a estrutura exigida
+
+/*pegar o número do cpf do input digitado
+verificar acho que com .leght se deve haver alguma caractere que não seja numero
+fazer um if e else que caso seja esteja correto ou falso apresentar a imagem de sim ou de não
+talvez uma div que mude no css de none para visivel*/
